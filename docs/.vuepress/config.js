b62f56fb546f9c405aa24a17eebeb61ef1708825
Psilocine <psilolau@foxmail.com>
2020-08-03 16:43:00 +0800

    docs: build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -82,11 +82,11 @@ module.exports = {
         {
           title: "关系判断",
           path: "/booleans/"
+          // },
+          // {
+          //   title: "单位换算",
+          //   path: "/unit_conversion/"
         }
-        // {
-        //   title: "单位换算类",
-        //   path: "/unit_conversion/"
-        // }
       ]
     }
   },
